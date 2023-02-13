782774b09cde36b3ecef062956a7ddaf5550f5ed
jwangsa <jwangsa@uwaterloo.ca>
2023-02-13 04:14:34 -0500

    Initial commit

---
+++ backend/data.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Madeleines',
+            category: 'Cake',
+            image: '/images/madeleines.jpg',
+            price: 11,
+            brand: 'Vanilla',
+            rating: 4.7,
+            numReviews: 10
+        },
+        {
+            _id: '2',
+            name: 'Cinnamon Rolls',
+            category: 'Cake',
+            image: '/images/cinnarolls.jpg',
+            price: 17,
+            brand: 'Cinnamon, Cream Cheese',
+            rating: 4.1,
+            numReviews: 3
+        },
+        {
+            _id: '3',
+            name: 'Strawberry Shortcake',
+            category: 'Cake',
+            image: '/images/strawberry_cake.jpg',
+            price: 23,
+            brand: 'Vanilla, Strawberry',
+            rating: 4.4,
+            numReviews: 2
+        },
+        {
+            _id: '4',
+            name: 'Black Sesame Cookies',
+            category: 'Cookies',
+            image: '/images/blackses_cookie.jpg',
+            price: 7,
+            brand: 'Black Sesame',
+            rating: 4.4,
+            numReviews: 2
+        },
+    ]
+}
